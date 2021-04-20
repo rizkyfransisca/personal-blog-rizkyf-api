dfdf3149654dd1feb8caef7698cb087d03b18bf4
rizkyfransisca <rizky.royal@gmail.com>
2021-04-20 22:29:44 +0700

    add database fixed postgres

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE personalblog
+-- CREATE DATABASE personalblog
 
 CREATE TABLE blogs(
     blog_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -6,7 +6,10 @@ CREATE TABLE blogs(
     author VARCHAR(255) NOT NULL,
     description VARCHAR NOT NULL,
     timeCreated DATE NOT NULL,
-    slug VARCHAR UNIQUE NOT NULL
+    slug VARCHAR UNIQUE NOT NULL,
+    image VARCHAR NOT NULL
 );
 
-ALTER TABLE blogs ADD COLUMN image VARCHAR NOT NULL;
+-- \q => to quit from postgresql heroku
+-- heroku pg:psql -a namawebapp(personal-blog-rizkyf-api)
+-- ALTER TABLE blogs ADD COLUMN image VARCHAR NOT NULL;
