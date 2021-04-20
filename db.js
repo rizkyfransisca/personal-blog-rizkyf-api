b3be32392c7ed3ad68c30ad88cd6989980af6b80
rizkyfransisca <rizky.royal@gmail.com>
2021-04-20 21:30:13 +0700

    fixing db config

--- db.js
+++ db.js
@@ -5,8 +5,8 @@ const devConfig = {
     user : process.env.PG_USER,
     password : process.env.PG_PASSWORD,
     host : process.env.PG_HOST,
-    port : process.env.PG_DATABASE,
-    database : process.env.PG_PORT
+    port : process.env.PG_PORT,
+    database : process.env.PG_DATABASE
 }
 
 const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig)
