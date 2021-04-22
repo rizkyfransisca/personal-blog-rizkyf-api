c6677cc8288c05c28181e86894d52c9e9b2263f5
rizkyfransisca <rizky.royal@gmail.com>
2021-04-22 14:22:31 +0700

    new welcome message

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 
 app.get("/", (req,res)=>{
-    res.json("Hallo Selamat Datang Di Personal Blog Pertama Saya")
+    res.json("Hallo Selamat Datang")
 })
 // app.get('/',(req,res)=>{
 //     // var today = new Date()
